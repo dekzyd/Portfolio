a2e407653a8a7f93ceddba104c7fd2ebcd6fbca2
dekzyd <adekoyadayo2010@gmail.com>
2023-07-03 23:13:31 +0100

    add animations

--- assets/js/main.js
+++ assets/js/main.js
@@ -183,5 +183,31 @@ themeButton.addEventListener("click", () => {
 });
 
 /*=============== CHANGE BACKGROUND HEADER ===============*/
-
+const scrollHeader = () => {
+  const header = document.getElementById("header");
+  // When the scroll is greater than 50 viewport height, add the bg-header class to the header tag
+  this.scrollY >= 50
+    ? header.classList.add("bg-header")
+    : header.classList.remove("bg-header");
+};
+window.addEventListener("scroll", scrollHeader);
 /*=============== SCROLL REVEAL ANIMATION ===============*/
+const sr = ScrollReveal({
+  origin: "top",
+  distance: "60px",
+  duration: 2000,
+  delay: 300,
+  // reset: true /* Animations repeat */
+});
+
+sr.reveal(
+  `.home__data, .projects__container, .footer__container, .testimonial__container`
+);
+sr.reveal(`.home__info div`, { delay: 400, origin: "bottom", interval: 100 });
+sr.reveal(`.skills__content:nth-child(1), .contact__content:nth-child(1)`, {
+  origin: "left",
+});
+sr.reveal(`.skills__content:nth-child(2), .contact__content:nth-child(2)`, {
+  origin: "right",
+});
+sr.reveal(`.qualification__content, .services__card`, { interval: 100 });
